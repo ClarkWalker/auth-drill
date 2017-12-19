846b712a200c9278e42c3842040b48f2c68ce096
Clark Walker <303walker@gmail.com>
2017-12-19 11:48:14 -0700

    more linting

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,40 @@
-var express = require('express')
-var router = express.Router()
-var db = require('../db/api')
-var bcrypt = require('bcrypt')
+var express = require('express');
+var router = express.Router();
+var db = require('../db/api');
+var bcrypt = require('bcrypt');
 
-router.post('/signin', function(req, res, next){
+router.post('/signin', function(req, res, next) {
   db.signIn()
-  .then(function(agent){
+  .then(function(agent) {
     //Use bcrypt to log in
-      if (isMatch) {
-        //Route to /Assignment
-      }
-      else {
-        res.render('index', { title: 'gClassified', message: 'Incorrect login. Contents will self destruct' })
-      }
-  })
-})
+    if (isMatch) {
+      //Route to /Assignment
+    } else {
+      res.render('index', {
+        title: 'gClassified',
+        message: 'Incorrect login. Contents will self destruct'
+      });
+    }
+  });
+});
 
-router.post('/signup', function(req,res,next){
+router.post('/signup', function(req, res, next) {
   //Use bcrypt to Sign Up
 
-    db.signUp()
-    .then(function(agent){
-      if (agent[0].password === req.body.password) {
-        res.render('index', { title: 'gClassified', message: 'Password Must Be Hashed. Government Secrets are at Stake!' })
-      }
-      else {
-        res.render('index', { title: 'gClassified', message: 'Sign Up Successful' })
-      }
-    })
-})
+  db.signUp()
+  .then(function(agent) {
+    if (agent[0].password === req.body.password) {
+      res.render('index', {
+        title: 'gClassified',
+        message: 'Password Must Be Hashed. Government Secrets are at Stake!'
+      });
+    } else {
+      res.render('index', {
+        title: 'gClassified',
+        message: 'Sign Up Successful'
+      });
+    }
+  });
+});
 
-module.exports = router
+module.exports = router;
